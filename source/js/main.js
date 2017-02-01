35f8215ac66b45fbefe03e2344726a02e8f662f6
Yuvv <yuvv_th@outlook.com>
2017-02-01 17:51:27 +0800

    commit my theme with my settings

---
+++ source/js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+    // set back to top
+    $('#btn-back-to-top').click(function(e){
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+    $(window).scroll(function() {
+        if ( $(window).scrollTop() > 300 ) {
+            $('#btn-back-to-top').fadeIn(100);
+        } else {
+            $('#btn-back-to-top').fadeOut(100);
+        }
+    });
+
+    // set pagination
+    $('.pagination').addClass('ui menu');
+    $('.pagination>a').addClass('item');
+    $('.pagination>span').addClass('item active');
+
+    // format post body
+    $('#post-body blockquote').addClass('ui piled segment');
+    $('#post-body img').addClass('ui rounded centered image');
+    $('#page-body').css('min-height',
+        window.innerHeight - $('#page-header').height() - $('#page-footer').height() - 20);
+});
